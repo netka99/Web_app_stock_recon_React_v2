0854a66a29e7df9a8bf37ad99d3d2a9cfc67bda8
Aneta <ajduka@gmail.com>
2024-10-09 12:29:49 +0200

    changing api structure of addresses

--- react-stock-react-only/src/pages/SettingsAddressPage.jsx
+++ react-stock-react-only/src/pages/SettingsAddressPage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { fetchData, updateDataOnApi } from '../api/fetchAPI'
+import shopImg from '../assets/store-img.svg'
+import editImg from '../assets/edit.png'
+import saveImg from '../assets/save-icon.png'
 
 import {
   Navbar,
@@ -18,6 +21,7 @@ const SettingsAddressPage = () => {
   const [settings, setSettings] = useState(null)
   const [addresses, setAddresses] = useState({})
   const [loading, setLoading] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
 
   const handleMessage = (messageType) => {
     getMessageText(messageType)
@@ -78,7 +82,33 @@ const SettingsAddressPage = () => {
         <div className="shopsListSettings">
           {settings ? (
             settings.shops.map((shop, index) => (
-              <div key={`${shop}_${index}`}>{shop} </div>
+              <div key={`${shop}_${index}`}>
+                <div className="shop-header">
+                  <img
+                    className="store-picture"
+                    src={shopImg}
+                    alt="shop image"
+                  />
+                  <span>{shop}</span>
+                </div>
+                {addresses[shop] ? (
+                  <div className="shop-address">
+                    {isEditing ? (
+                      <textarea
+                        type="text"
+                        defaultValue={addresses[shop]}
+                        onChange={(e) => e.target.value}
+                      />
+                    ) : (
+                      <div>
+                        <div>{addresses[shop]}</div>
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <div>brak adresu</div>
+                )}
+              </div>
             ))
           ) : (
             <Spinner />
@@ -125,6 +155,58 @@ const Container = styled.div`
     margin-bottom: 1rem;
     opacity: 1;
     transition: opacity 0.3s ease-in-out;
+  }
+  .store-picture {
+    width: 24px;
+    margin: 8px;
+    filter: invert(41%) sepia(0%) saturate(0%)
+      hue-rotate(102deg) brightness(91%) contrast(85%);
+  }
+  .edit-button,
+  .delete-button {
+    margin-left: auto;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+
+    @media screen and (max-width: $mobileL) {
+      padding-right: 0rem;
+    }
+    img {
+      display: block;
+      /* width: 90%;
+      height: 90%; */
+    }
+  }
+
+  .edit-button {
+    padding-left: 2rem;
+    img {
+      filter: brightness(0) saturate(100%) invert(65%)
+        sepia(45%) saturate(5826%) hue-rotate(176deg)
+        brightness(97%) contrast(92%);
+    }
+    &:focus,
+    &:hover {
+      filter: brightness(0) saturate(100%) invert(13%)
+        sepia(92%) saturate(4907%) hue-rotate(228deg)
+        brightness(88%) contrast(105%);
+    }
+  }
+
+  .delete-button {
+    padding-right: 2rem;
+    img {
+      filter: brightness(0) saturate(100%) invert(33%)
+        sepia(87%) saturate(3694%) hue-rotate(335deg)
+        brightness(99%) contrast(89%);
+    }
+    &:focus,
+    &:hover {
+      filter: brightness(0) saturate(100%) invert(30%)
+        sepia(76%) saturate(2026%) hue-rotate(335deg)
+        brightness(72%) contrast(96%);
+    }
   }
 `
 
